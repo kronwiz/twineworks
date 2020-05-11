70f4dffce8bab50ec6fbb2f4c27a4ba6ef19217a
Andrea Galimberti <andrea.galimberti@gmail.com>
2020-05-11 14:55:43 +0200

    Added license.

--- sugarcube_macros/setcontent/setcontent.js
+++ sugarcube_macros/setcontent/setcontent.js
@@ -1,4 +1,20 @@
-/* setmultiline */
+/*
+    Copyright Kronwiz.
+    This file is part of https://github.com/kronwiz/twineworks repository.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 Macro.add('setcontent', {
 	tags     : [],
