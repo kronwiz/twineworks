99e3a1f2b2d4131b4a63fef6d22e51d7a76d83cb
Andrea Galimberti <andrea.galimberti@gmail.com>
2020-05-11 10:07:51 +0200

    New "setwiki" macro.

---
+++ sugarcube_macros/setwiki/setwiki.js
@@ -0,0 +1,23 @@
+/* setmultiline */
+
+Macro.add('setwiki', {
+	tags     : [],
+	handler  : function () {
+			if (this.args.length < 1) return this.error("missing variableName parameter");
+			var var_name = this.args[0];
+
+			if (var_name === undefined) return this.error('probably the quotes around "' + this.args.raw + '" are missing');
+
+			if (var_name.match(/^\$/)) {
+				variables()[var_name.replace(/^\$/, "")] = this.payload[0].contents;
+			}
+			else if (var_name.match(/^_/)) {
+				temporary()[var_name.replace(/^_/,"")] = this.payload[0].contents;
+			} else {
+				return this.error("the variableName parameter must start with '$' or '_'");
+			}
+
+			console.log(variables());
+			console.log(temporary());
+	}
+})
